62e533e84ca928a70c5e0b5285a97a7f3b501d63
Oleksandr Myronovych <alexmyronovych@gmail.com>
2020-12-05 15:25:29 +0200

    init

---
+++ ParimatchHW1/ParimatchHW/FactorialVC.swift
@@ -0,0 +1,20 @@
+//
+//  FactorialVC.swift
+//  ParimatchHW
+//
+//  Created by rs on 05.12.2020.
+//  Copyright © 2020 Oleksandr Myronovych. All rights reserved.
+//
+
+import UIKit
+
+class FactorialVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    }
+
+
+}
+
