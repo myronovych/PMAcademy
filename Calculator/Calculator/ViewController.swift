188f4dac04f070c6b8eed4487a04f4ac130a15e3
Oleksandr Myronovych <alexmyronovych@gmail.com>
2020-12-19 00:40:57 +0200

    Calculator done

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Oleksandr Myronovych on 18.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
